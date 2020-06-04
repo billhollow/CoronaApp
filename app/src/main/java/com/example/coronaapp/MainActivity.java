4862a06abb3d39a5fc073ce7980ee8860df68440
LAPTOP-RA3FA7T0\Piero <pcruzmam@unsa.edu.pe>
2020-06-03 22:39:30 -0500

    Initial commit

---
+++ app/src/main/java/com/example/coronaapp/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.coronaapp;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.coronaapp.model.Noticia;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+    //Declaración variables a utilizar
+    EditText titulo,tema,descripcion;
+    Button aceptar;
+    FirebaseDatabase database;
+    DatabaseReference myRef;
+    Noticia noticia;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Extracción de datos
+        titulo=(EditText)findViewById(R.id.txt_titulo);
+        tema=(EditText)findViewById(R.id.txt_tema);
+        descripcion=(EditText)findViewById(R.id.txt_descripcion);
+        aceptar=(Button)findViewById(R.id.aceptar);
+        noticia = new Noticia();
+
+        //conexión con la base de datos
+        database = FirebaseDatabase.getInstance();
+        myRef= FirebaseDatabase.getInstance().getReference().child("Noticia");
+
+        //listener boton aceptar
+        aceptar.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                String tit = titulo.getText().toString();
+                String tem = tema.getText().toString();
+                String des = descripcion.getText().toString();
+                noticia.setTitulo(tit);
+                noticia.setTema(tem);
+                noticia.setDescripcion(des);
+                //databaseReference.child("Noticia").child(noticia.getUid()).setValue(noticia);
+                myRef.push().setValue(noticia);
+                Toast.makeText(MainActivity.this, "agregado", Toast.LENGTH_LONG).show();
+
+
+            }
+        });
+
+
+
+    }
+}
---
+++ app/src/main/java/com/example/coronaapp/model/Noticia.java
@@ -0,0 +1,48 @@
+package com.example.coronaapp.model;
+
+public class Noticia {
+    private String uid;
+    private String titulo;
+    private String tema;
+    private String descripcion;
+
+    public Noticia() {
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getTema() {
+        return tema;
+    }
+
+    public void setTema(String tema) {
+        this.tema = tema;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    @Override
+    public String toString() {
+        return this.titulo;
+    }
+}
